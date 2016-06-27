40b1a5eea2061d99249546e332c7a0dce37ded53
eitabilly <longlongago003@gmail.com>
2016-06-27 20:07:34 +0900

    add TitleScene.cpp

---
+++ Classes/TitleScene.cpp
@@ -0,0 +1,9 @@
+//
+//  TitleScene.cpp
+//  CurlingGame
+//
+//  Created by 石田栄太 on 2016/06/27.
+//
+//
+
+#include "TitleScene.hpp"
---
+++ Classes/TitleScene.hpp
@@ -0,0 +1,14 @@
+//
+//  TitleScene.hpp
+//  CurlingGame
+//
+//  Created by 石田栄太 on 2016/06/27.
+//
+//
+
+#ifndef TitleScene_hpp
+#define TitleScene_hpp
+
+#include <stdio.h>
+
+#endif /* TitleScene_hpp */
