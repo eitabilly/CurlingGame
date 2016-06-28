92a5d079b68335f2bc36f9e5d5693300a05bbd3f
eitabilly <longlongago003@gmail.com>
2016-06-28 17:48:39 +0900

    TitleSceneに背景、タイトルロゴ、スタートボタンを追加

--- Classes/TitleScene.hpp
+++ Classes/TitleScene.hpp
@@ -9,6 +9,20 @@
 #ifndef TitleScene_hpp
 #define TitleScene_hpp
 
-#include <stdio.h>
+#include "cocos2d.h"
+#include "ui/cocosGUI.h"
+
+class TitleScene:public cocos2d::Layer
+{
+public:
+    //There's no 'id' in cpp. so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+    
+    //Here's a difference. Method 'init' in cocos2d-x returns bool. instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+    
+    //Implement the "static create()" method manually
+    CREATE_FUNC(TitleScene);
+};
 
 #endif /* TitleScene_hpp */
