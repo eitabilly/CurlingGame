bda62bc604c88008093bc8acbe35ba70729f9c72
eitabilly <longlongago003@gmail.com>
2016-06-30 19:39:49 +0900

    GameScene.cpp add

---
+++ Classes/GameScene.hpp
@@ -0,0 +1,14 @@
+//
+//  GameScene.hpp
+//  CurlingGame
+//
+//  Created by 石田栄太 on 2016/06/30.
+//
+//
+
+#ifndef GameScene_hpp
+#define GameScene_hpp
+
+#include <stdio.h>
+
+#endif /* GameScene_hpp */
