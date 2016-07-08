0ebb60ef9f29a2fdca8852d9b2d0ee475667b694
eitabilly <longlongago003@gmail.com>
2016-07-08 23:50:37 +0900

    physics stone add start

---
+++ Classes/Stone.cpp
@@ -0,0 +1,9 @@
+//
+//  Stone.cpp
+//  CurlingGame
+//
+//  Created by 石田栄太 on 2016/07/08.
+//
+//
+
+#include "Stone.hpp"
---
+++ Classes/Stone.hpp
@@ -0,0 +1,14 @@
+//
+//  Stone.hpp
+//  CurlingGame
+//
+//  Created by 石田栄太 on 2016/07/08.
+//
+//
+
+#ifndef Stone_hpp
+#define Stone_hpp
+
+#include <stdio.h>
+
+#endif /* Stone_hpp */
